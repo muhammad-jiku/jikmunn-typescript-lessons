a66dc184031dd5e42ab73b3bd36a7835a219e2e6
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-27 13:53:40 +0600

    7. any types

---
+++ 7. any/index.js
@@ -0,0 +1,10 @@
+var password;
+var passwords;
+password = "6523fghajk";
+console.log(password);
+password = true;
+console.log(password);
+password = 64523;
+console.log(password);
+passwords = ["agsh", 6523, false];
+console.log(passwords);
---
+++ 7. any/index.ts
@@ -0,0 +1,14 @@
+let password: any;
+let passwords: any[];
+
+password = "6523fghajk";
+console.log(password);
+
+password = true;
+console.log(password);
+
+password = 64523;
+console.log(password);
+
+passwords = ["agsh", 6523, false];
+console.log(passwords);
