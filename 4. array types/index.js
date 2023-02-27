ad9f717f3e3bb48cf6fd6fe809d8819db2b7e6b0
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-27 12:53:40 +0600

    4. Array types

---
+++ 4. array types/index.js
@@ -0,0 +1,10 @@
+// let comics : string[]
+var comics;
+comics = ["dc", "marvel", "chacha chowdhury"];
+comics.sort();
+console.log(comics);
+console.log("------------------------------------------");
+var multiplel;
+multiplel = ["string", "number", 64856];
+multiplel.sort();
+console.log(multiplel);
---
+++ 4. array types/index.ts
@@ -0,0 +1,15 @@
+// let comics : string[]
+let comics: Array<string>;
+
+comics = ["dc", "marvel", "chacha chowdhury"];
+
+comics.sort();
+console.log(comics);
+
+console.log("------------------------------------------");
+let multiplel: (string | number)[];
+
+multiplel = ["string", "number", 64856];
+
+multiplel.sort();
+console.log(multiplel);
