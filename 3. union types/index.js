c82ce69bd465faaae2e4dbdeecde6511a7007d36
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-27 12:29:37 +0600

    3. union types

---
+++ 3. union types/index.js
@@ -0,0 +1,7 @@
+var userId;
+userId = 16526;
+console.log("first", userId);
+userId = "64563";
+console.log("second", userId);
+userId = false;
+console.log("third", userId);
---
+++ 3. union types/index.ts
@@ -0,0 +1,10 @@
+let userId: string | number | boolean;
+
+userId = 16526;
+console.log("first", userId);
+
+userId = "64563";
+console.log("second", userId);
+
+userId = false;
+console.log("third", userId);
