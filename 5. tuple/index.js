65df7577d415df8c816e0214355e18ca0f6b8807
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-27 13:08:17 +0600

    5.tuple types

---
+++ 5. tuple/index.js
@@ -0,0 +1,8 @@
+var users;
+users = [101, "robert downey jr."];
+console.log(users);
+console.log(users[0]);
+console.log(users[1]);
+users.push(102, "shakib al hasan");
+console.log(users);
+//  Tuple: A tuple is a type that represents an array with a fixed number of elements, where each element may have a different type. Tuples are useful for representing fixed-length data structures where each element has a specific type. However, they can be less flexible than arrays, as their length and element types are fixed at the time of declaration.
