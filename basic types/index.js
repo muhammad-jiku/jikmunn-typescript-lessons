aadaaa2ef27b7f364e3ef8928073aad17874ab20
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-27 12:15:54 +0600

    2. data types

---
+++ basic types/index.js
@@ -0,0 +1,7 @@
+var userId;
+var username;
+var userVerified;
+userId = 15232;
+username = "shang-chi";
+userVerified = true;
+console.log("".concat(username.toLowerCase(), " is now verified by userId no.").concat(userId, ", which is ").concat(userVerified, "."));
---
+++ basic types/index.ts
@@ -0,0 +1,11 @@
+let userId: number;
+let username: string;
+let userVerified: boolean;
+
+userId = 15232;
+username = "shang-chi";
+userVerified = true;
+
+console.log(
+  `${username.toLowerCase()} is now verified by userId no.${userId}, which is ${userVerified}.`
+);
